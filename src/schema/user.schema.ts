58e3d29d0b31f8658264194b7e2b33de6b3572c7
Rabii Luena <itisrabii@gmail.com>
2022-12-19 22:35:55 +0300

    fix: missing types for login

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -4,3 +4,12 @@ export const createUserSchema = z.object({
   name: z.string().min(3),
   email: z.string().min(4).email(),
 });
+
+export const loginUserOTPSchema = z.object({
+  email: z.string().min(4).email(),
+  redirect: z.string().default("/"),
+});
+
+export const verifyTokenSchema = z.object({
+  token: z.string().min(1),
+});
